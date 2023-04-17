c0eccc0bbcccc54203e2ec4a8aa302cd11eafe6e
unknown <kananng@code.edu.az>
2023-04-17 14:40:03 +0400

    Homework

---
+++ bufer/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+
+//int[,] numbers =
+//    {
+//    {16,17 },
+//    { 18,19 },
+//    { 26,82}
+
+//    };
+
+//for (int i = 0; i < 3; i++)
+//{
+//    for (int j = 0; j < 2; j++)
+//    {
+//        Console.WriteLine(numbers[i, j]);
+//    }
+
+//}
